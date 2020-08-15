f06ce2cad8bafe77f29fd32427e66ba2c51000cb
i1-kmv <i1-kmv@mail.ru>
2020-08-15 14:22:29 +0200

    блочная обработка ошибок

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -2,53 +2,54 @@ import React, { Component } from 'react';
 
 import './person-details.css';
 import SwapiService from "../../services/swapi-service";
-import Spinner from "../spinner";
+import ErrorButton from "../error-button/error-button";
 
 export default class PersonDetails extends Component {
 
-  swapiService = new SwapiService()
+  swapiService = new SwapiService();
 
   state = {
     person: null
-  }
+  };
 
   componentDidMount() {
-    this.updatePerson()
+    this.updatePerson();
   }
 
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.personId != prevProps.personId) {
-      this.updatePerson()
+  componentDidUpdate(prevProps) {
+    if (this.props.personId !== prevProps.personId) {
+      this.updatePerson();
     }
   }
 
   updatePerson() {
-    const { personId } = this.props
+    const { personId } = this.props;
     if (!personId) {
-      return
+      return;
     }
+
     this.swapiService
-        .getPerson(personId)
-        .then((person) => {
-          this.setState({person})
-        })
+      .getPerson(personId)
+      .then((person) => {
+        this.setState({ person });
+      });
   }
 
   render() {
 
-    if (!this.state.person) {
-      return <span>Select a person from a list</span>
+    const { person } = this.state;
+    if (!person) {
+      return <span>Select a person from a list</span>;
     }
 
-    const {
-            id, name, gender, birthYear, eyeColor
-    } = this.state.person
-
+    const { id, name, gender,
+              birthYear, eyeColor } = person;
 
     return (
       <div className="person-details card">
         <img className="person-image"
-          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          alt="character"/>
 
         <div className="card-body">
           <h4>{name}</h4>
@@ -66,6 +67,7 @@ export default class PersonDetails extends Component {
               <span>{eyeColor}</span>
             </li>
           </ul>
+          <ErrorButton />
         </div>
       </div>
     )

46f8eb31df0abb0c0dcedf637a0087f6cfc0f403
i1-kmv <i1-kmv@mail.ru>
2020-09-07 14:41:23 +0200

    Updates

---
+++ precache-manifest.3fab18af6e2415fcfddaceb1b1327e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d6d57a77554b881e2c32a8440660311",
+    "url": "/starDB/index.html"
+  },
+  {
+    "revision": "99e238ddba8223743d33",
+    "url": "/starDB/static/css/main.4bc815f4.chunk.css"
+  },
+  {
+    "revision": "c702c5d0646a38e45fcc",
+    "url": "/starDB/static/js/2.43d23851.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starDB/static/js/2.43d23851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e238ddba8223743d33",
+    "url": "/starDB/static/js/main.c7056f09.chunk.js"
+  },
+  {
+    "revision": "1bc32fcecec408043361",
+    "url": "/starDB/static/js/runtime-main.47fb1933.js"
+  }
+]);
